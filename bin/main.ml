9a89243a43a6deb32edc976c95737ced546ed0b1
lucas-cauhe <lcauhe@gmail.com>
2023-07-24 21:11:10 +0200

    add method working as it should

--- bin/main.ml
+++ bin/main.ml
@@ -6,36 +6,99 @@ open Bd_b_plus.Add
 
 exception BadTree
 
+let arrays_differ_at_pos ar1 ar2 = 
+  let len1 = Array.length ar1 in
+  let len2 = Array.length ar2 in
+  if len1 = 0 || len2 = 0 then
+    1
+  else
+    let found = ref false in
+    let mn = min len1 len2 in
+    let pos = ref (-1) in
+    let () = for i = 0 to mn-1 do
+      if not (ar1.(i) = ar2.(i)) then found := true; pos := i
+    done
+    in
+    if !found then !pos
+    else mn
 
+let rec update_leafs n t inserted_key = 
+  match t with
+  | (Node [(Nil, _, None, ind)]) as c when ind < n -> c
+  | Node [(Nil, key, None, ind)] -> 
+    if not (inserted_key = key) then
+      Node [(Nil, key, None, ind+1)]
+    else Node [(Nil, key, None, ind)]
 
+  | Node ((Nil, _, _, ind) as h :: l) when ind < n -> 
+    let updated = match update_leafs n (Node l) inserted_key with 
+      | Node s -> s
+      | _ -> raise BadTree
+    in
+    Node (h::updated)
+
+  | Node ((Nil, key, v, ind) as h :: l) ->
+    let updated = match update_leafs n (Node l) inserted_key with
+      | Node s -> s
+      | _ -> raise BadTree
+    in
+    if not (inserted_key = key) then
+      Node ((Nil, key, v, ind+1)::updated)
+    else Node (h::updated)
+  
+  | Node [(ch, k, None, i)] ->
+    let ch_updated = update_leafs n ch inserted_key in
+    Node [(ch_updated, k, None, i)]
+    
+  | Node ((ch, k, None, i) :: l) -> 
+    let ch_updated = update_leafs n ch inserted_key in
+    let rest_updated = match update_leafs n (Node l) inserted_key with 
+      | Node s -> s
+      | _ -> raise BadTree
+    in
+    Node ((ch_updated, k, None, i) :: rest_updated)
+  
+  | Nil -> t
+
+  | _ -> t
 
 module Db = struct 
   
-  let empty = Nil
+  let empty = (Nil, [||])
+    
   let add t v = 
+    let key = get_pkey (Nil, v, None, 0) in
+    let (prev_t, prev_arr) = t in
     match Bd_b_plus.Add.add t v with
-    | (Some t', _) -> t'
-    | (None, _) -> t
+    | ((Some t', _), arr) -> 
+      (* update the leaf nodes after the added one *)
+      let final_tree = update_leafs (arrays_differ_at_pos prev_arr arr) t' key in
+      (final_tree, arr)
+    | ((None, _), arr) -> (prev_t, arr)
 
   let _delete _ tree = tree
   let _search _ _ = Some 1
-  let rec draw t = 
+  let rec draw_tree t = 
     match t with
     | Nil
-    | Node [(Nil, _, None)] -> Printf.printf "\n"
-    | Node ((Nil, key, Some _) :: l) ->
+    | Node [(Nil, _, None, _)] -> Printf.printf "\n"
+    | Node ((Nil, key, Some _, _) :: l) ->
       Printf.printf "Hoja %d " key;
-      draw (Node l)
-    | Node [(ch, _, None)] ->
+      draw_tree (Node l)
+    | Node [(ch, _, None, _)] ->
         Printf.printf "---- Hijos de centinela ----\n";
-        draw ch;
+        draw_tree ch;
         Printf.printf "---- Fin hijos de centinela ----\n"
-    | Node ((ch, key, None) :: l) ->
+    | Node ((ch, key, None, _) :: l) ->
         Printf.printf "---- Hijos de %d ----\n" key;
-        draw ch;
+        draw_tree ch;
         Printf.printf "---- Fin hijos de %d ----\n" key;
-        draw (Node l)
+        draw_tree (Node l)
     | _ -> raise BadTree
+  
+  let draw_leafs a = 
+    Array.iter (fun x -> Printf.printf "%d -> " x) a;
+    print_endline ""
 end
 
 exception StopMain
@@ -47,14 +110,17 @@ let process_query c v t =
   | _ -> t
 
 let () = 
-  let tree = ref Db.empty in
+  let (tree, arr) = Db.empty in
+  let (tree, arr) = (ref tree, ref arr) in
   try
     while true do
       let tokenized = String.split_on_char ' ' (read_line ()) in
       let code = int_of_string (List.nth tokenized 0) in
       let key_or_value = int_of_string (List.nth tokenized 1) in
-      tree := process_query code key_or_value !tree;
-      Db.draw !tree
+      let (t, a) = process_query code key_or_value (!tree, !arr) in
+      tree := t; arr := a;
+      Db.draw_tree !tree;
+      Db.draw_leafs !arr
     done
   with StopMain -> print_endline "stopped execution";
 
--- lib/add.ml
+++ lib/add.ml
@@ -1,16 +1,18 @@
-type database = Nil | Node of (database * int * int option) list
+(* tuple contains: (recursive children, key, possible value, number of leafs to the left) *)
+type database = Nil | Node of (database * int * int option * int) list
 
-(* let get_pkey (v: int) = v
-let get_pkey (v: string) = v *)
-let get_pkey (v: database * int * int option) = 
-  let (_, key, _) = v in
+(* you really dont care about the 4th element of the Node list tuple in non leafs nodes
+   since only the leafs have useful information there *)
+
+let get_pkey (v: database * int * int option * int) = 
+  let (_, key, _, _) = v in
   key
 
-let (>>=) (m : database option * bool) (k : database * bool -> database option * bool) : database option * bool =
-  let (datb, moved) = m in
+let (>>=) (m : (database option * bool) * int array) (k : (database * bool) * int array -> (database option * bool) * int array) : (database option * bool) * int array =
+  let ((datb, moved), res_llist) = m in
   match datb with
-  | None -> (None, moved)
-  | Some x -> k (x, moved)
+  | None -> ((None, moved), res_llist)
+  | Some x -> k ((x, moved), res_llist)
 
 let rec take (l: 'a list) n =
   match l with
@@ -30,64 +32,71 @@ let rec drop l n =
 let divide l = 
   let third_pkey = get_pkey (List.nth l 2) in
   let (cent, taker) = match (List.nth l 2) with
-  | (Nil, _, _) -> ((Nil, third_pkey, None), 2)
-  | (node, _, _) -> ((node, third_pkey, None), 3) in
+  | (Nil, _, _, _) -> ((Nil, third_pkey, None, 0), 2)
+  | (node, _, _, _) -> ((node, third_pkey, None, 0), 3) in
   let first_half = (take l 2) @ [cent] (*[l.(0), l.(1)] + centinela *) in
   let second_half = drop l taker (*[l.(3), l.(4)]*) in
   Node [
-    (Node first_half, third_pkey, None);
-    (Node second_half, third_pkey (* this key is never going to beread *), None)
+    (Node first_half, third_pkey, None, 0);
+    (Node second_half, third_pkey (* this key is never going to beread *), None, 0)
   ]
 
-let add t (v: int) = 
-  let key = get_pkey (Nil, v, None) in
+let insert_at pos ar v = 
+  let len = Array.length ar in
+  Array.init (len+1) (fun ind -> 
+    if ind = pos then v
+    else if ind < pos then ar.(ind)
+    else ar.(ind-1) )
+
+let add (t, llist) (v: int) : (database option * bool) * int array = 
+  let key = get_pkey (Nil, v, None, 0) in
   let max_degree = 3 in
-  let rec aux t' reg_length : database option * bool (* (árbol resultante,
+  let rec aux t' reg_length : (database option * bool) * int array (* (árbol resultante,
   ha_cambiado el hijo *) = 
     match t' with
-    | Nil (* arbol vacío *) -> (Some (Node [(Nil, key, Some v); (Nil, key, None)]), false)
+    | Nil (* arbol vacío *) -> ((Some (Node [(Nil, key, Some v, 0); (Nil, key, None, 0)]), false), Array.make 1 v)
 
-    | Node ((_, k, _) :: _) when k=key (* clave primaria duplicada *) -> (None, false) 
+    | Node ((_, k, _, _) :: _) when k=key (* clave primaria duplicada *) -> ((None, false),  llist)
 
-    | Node ((Nil, k, Some _) as h :: l) when key < k (* Añadir nuevo objeto aquí *) ->
+    | Node ((Nil, k, Some _, hleft) as h :: l) when key < k (* Añadir nuevo objeto aquí *) ->
         if (List.length l) + reg_length = max_degree then
           if reg_length = 0 then (* dividir y coger indice 2 *)
-            (Some (divide ([(Nil, key, Some v); h]@l)), true)
-          else (Some (Node ([(Nil, key, Some v); h]@l)), true)
+            ((Some (divide ([(Nil, key, Some v, hleft); h]@l)), true), insert_at hleft llist v)
+          else ((Some (Node ([(Nil, key, Some v, hleft); h]@l)), true), insert_at hleft llist v)
         else
           if reg_length = 0 then
-            (Some (Node ([(Nil, key, Some v); h]@l)), false)
-          else (Some (Node ([(Nil, key, Some v); h]@l)), false) 
+            ((Some (Node ([(Nil, key, Some v, hleft); h]@l)), false), insert_at hleft llist v)
+          else ((Some (Node ([(Nil, key, Some v, hleft); h]@l)), false), insert_at hleft llist v)
 
-    | Node ((Nil, _, Some _) as h :: l) (* Avanza en la lista l *) -> 
-       aux (Node l) (reg_length + 1) >>= (fun (prt, moved) -> (* resulting tree *)
+    | Node ((Nil, _, Some _, _) as h :: l) (* Avanza en la lista l *) -> 
+       aux (Node l) (reg_length + 1) >>= (fun ((prt, moved), res_llist) -> (* resulting tree *)
           let rt = match prt with 
           | Node s -> s
           | _ -> raise B_plus_sig.BadTree
         in
          if moved && reg_length = 0 then (* you are at the first element *)
           (*divide the current tree and take the key of the third element*)
-          (Some (divide (h::rt) ), true)
-         else if moved && not (reg_length = 0) then (Some (Node (h::rt)), true)
+          ((Some (divide (h::rt) ), true), res_llist)
+         else if moved && not (reg_length = 0) then ((Some (Node (h::rt)), true), res_llist)
           else if not moved && reg_length = 0 then
-            (Some (Node (h::rt)), false)
-         else (Some (Node (h::rt)), false) 
+            ((Some (Node (h::rt)), false), res_llist)
+         else ((Some (Node (h::rt)), false), res_llist) 
         )
-    | Node [(Nil, _, None) as lst] (* Centinela, añadir aquí y mover el Centinela *) -> 
+    | Node [(Nil, _, None, hleft)] (* Centinela, añadir aquí y mover el Centinela *) -> 
         if reg_length = max_degree then (* Divides en dos y propagas *)
           (* El árbol devuelto tendrá como raíz la clave del tercer elemento e
            * hijos el registro acutal a la mitad *)
 
-          (Some (Node [(Nil, key, Some v); lst]) , true)
+          ((Some (Node [(Nil, key, Some v, hleft+1); (Nil, key, None, hleft+1)]), true), insert_at (hleft+1) llist v)
         else (* Mueves el centinela *)
-          (Some (Node [(Nil, key, Some v); lst]), false)
+          ((Some (Node [(Nil, key, Some v, hleft+1); (Nil, key, None, hleft+1)]), false), insert_at (hleft+1) llist v)
     
     (*-------------------------------*)
     (* Casos para los nodos no hojas *)
     (*-------------------------------*)
 
-    | Node [(s, k, None)] ->
-      aux s 0 >>= (fun (prt, moved) ->
+    | Node [(s, k, None, _)] ->
+      aux s 0 >>= (fun ((prt, moved), res_llist) ->
         let rt = match prt with 
           | Node s -> Node s
           | _ -> raise B_plus_sig.BadTree
@@ -95,45 +104,45 @@ let add t (v: int) =
         if moved then
             let (new_element, prev_el_new_tree) = begin
               match rt with 
-              | Node [(Node _, _, None) as c'; (sh, _, None)] -> (c', sh)
+              | Node [(Node _, _, None, _) as c'; (sh, _, None,_)] -> (c', sh)
               | _ -> raise B_plus_sig.BadTree
             end
             in
-            let result_list = [new_element; (prev_el_new_tree, k, None)]
+            let result_list = [new_element; (prev_el_new_tree, k, None, 0)]
             in
             if reg_length = 0 then
-              (Some (Node result_list), false)
+              ((Some (Node result_list), false), res_llist)
             else
-              (Some (Node result_list), reg_length+1 > 3)
+             ((Some (Node result_list), reg_length+1 > 3), res_llist)
         else
           if reg_length = 0 then
-           (Some (Node [(rt, k, None)]), false)
+           ((Some (Node [(rt, k, None, 0)]), false), res_llist)
           else 
-            (Some (Node [(rt, k, None)]), false)
+            ((Some (Node [(rt, k, None, 0)]), false), res_llist)
       ) 
 
-    | Node ((_, k, None) as h :: l) when key > k -> (* Avanza en la lista *)
-        aux (Node l) (reg_length+1) >>= (fun (prt, moved) ->
+    | Node ((_, k, None, _) as h :: l) when key > k -> (* Avanza en la lista *)
+        aux (Node l) (reg_length+1) >>= (fun ((prt, moved), res_llist) ->
           let rt = match prt with 
           | Node s -> s
           | _ -> raise B_plus_sig.BadTree
         in
           if moved && reg_length = 0 then
-            (Some (divide (h::rt)), true)
+            ((Some (divide (h::rt)), true), res_llist)
           else if moved && not (reg_length = 0) then
-            (Some (Node (h::rt)), true)
+            ((Some (Node (h::rt)), true), res_llist)
           else if not moved && reg_length = 0 then
-            (Some (Node (h::rt)), false)
+            ((Some (Node (h::rt)), false), res_llist)
           else
-            (Some (Node (h::rt)), false)
+            ((Some (Node (h::rt)), false), res_llist)
         )
-    | Node ((s, k, None) :: l) -> (* Investiga por debajo *)
+    | Node ((s, k, None, _) :: l) -> (* Investiga por debajo *)
 (*the first to receive moved=true, means its child got moved, update
            * current register with resulting tree and then check if the
            * resulting register needs to get splitted eventually returning a
            * true moved value *)
 
-        aux s 0 >>= (fun (prt, moved) ->
+        aux s 0 >>= (fun ((prt, moved), res_llist) ->
           let rt = match prt with 
           | Node s -> Node s
           | _ -> raise B_plus_sig.BadTree
@@ -141,24 +150,24 @@ let add t (v: int) =
           if moved then
             let (new_element, prev_el_new_tree) = begin
               match rt with 
-              | Node [(Node _, _, None) as c'; (sh, _, None)] -> (c', sh)
+              | Node [(Node _, _, None, _) as c'; (sh, _, None, _)] -> (c', sh)
               | _ -> raise B_plus_sig.BadTree
             end
             in
-            let result_list = [new_element; (prev_el_new_tree, k, None)]@l in
+            let result_list = [new_element; (prev_el_new_tree, k, None, 0)]@l in
             if reg_length = 0 then
               if List.length result_list > 4 then
-                (Some (divide result_list), true)
+                ((Some (divide result_list), true), res_llist)
               else
-                (Some (Node result_list), false) (*Aquí cortas la propagación del moved*)
+                ((Some (Node result_list), false), res_llist) (*Aquí cortas la propagación del moved*)
             else
               let moving = reg_length + (List.length result_list) > 4 in
-              (Some (Node result_list), moving)
+              ((Some (Node result_list), moving), res_llist)
           else 
             if reg_length = 0 then
-            (Some (Node ([(rt, k, None)]@l)), false)
+            ((Some (Node ([(rt, k, None, 0)]@l)), false), res_llist)
             else
-            (Some (Node ([(rt, k, None)]@l)), false)
+            ((Some (Node ([(rt, k, None, 0)]@l)), false), res_llist)
         )
     
     | _ (* Resto de casos por arbol mal construido *) -> raise B_plus_sig.BadTree 
